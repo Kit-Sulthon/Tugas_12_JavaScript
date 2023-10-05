d9d670048fa52fbb06bbc0c8e968cb839c3a6568
T R A $ H <sulthons.a19@gmail.com>
2023-10-05 15:37:51 +0700

    Tugas 12 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas12.js
@@ -0,0 +1,15 @@
+function Tugas12() {
+  var data = ["Pisang", "Jeruk", "Apel", "Mangga"];
+  console.log(data);
+  console.log("===================================");
+  //Array Shifr
+  data.shift();
+  console.log(data);
+  console.log("==================================");
+  //Array POP
+  data.pop();
+
+  //
+  return data;
+}
+console.log(Tugas12());
